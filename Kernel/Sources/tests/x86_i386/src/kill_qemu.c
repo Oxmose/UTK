73374f1bfaed48fafb18f8e7fb1a2e66e8eecb13
Oxmose <alexy.torresa@gmail.com>
2021-12-23 23:53:24 -0500

    Updated test kill_qemu definition

--- Kernel/Sources/tests/x86_i386/src/kill_qemu.c
+++ Kernel/Sources/tests/x86_i386/src/kill_qemu.c
@@ -1,7 +1,9 @@
 #ifdef TEST_MODE_ENABLED
 
+void kill_qemu(void);
+
 void kill_qemu(void)
-{   
+{
     while(1)
     {
         __asm__ __volatile__("outw %w0, %w1" : : "ax" (0x2000), "Nd" (0x604));
